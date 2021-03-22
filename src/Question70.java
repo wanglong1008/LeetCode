8cc1a13bd3fe3c1786c437e2c3059a0170fabe0c
wanglong1008 <925367077@qq.com>
2021-03-22 12:55:56 +0800

    v0.1

---
+++ src/Question70.java
@@ -0,0 +1,16 @@
+public class Question70 {
+    public int climbStairs(int n) {
+        if(n<=2){
+            return n;
+        }
+        int i1 = 1;
+        int i2 = 2;
+        for(int i=3;i<=n;i++){
+            int temp = i1+i2;
+            i1 = i2;
+            i2 = temp;
+        }
+        return i2;
+
+    }
+}
