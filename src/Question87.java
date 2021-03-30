e97fcf79bcb2de4a86f0e83c618cbfaf3a73ec41
wanglong1008 <925367077@qq.com>
2021-03-30 12:34:15 +0800

    v0.1

---
+++ src/Question87.java
@@ -0,0 +1,23 @@
+public class Question87 {
+    public boolean isScramble(String s1, String s2) {
+        if (s1.equals(s2))
+            return true;
+        int n = s1.length();
+        int memo[] = new int[26];
+        for (int i = 0; i < n; ++i) {
+            ++memo[s1.charAt(i) - 'a'];
+            --memo[s2.charAt(i) - 'a'];
+        }
+        for (int i = 0; i < 26; ++i) {
+            if (memo[i] != 0)
+                return false;
+        }
+        for (int i = 1; i < n; ++i) {
+            if ((isScramble(s1.substring(0, i), s2.substring(0, i)) && isScramble(s1.substring(i), s2.substring(i))) ||
+                    (isScramble(s1.substring(0, i), s2.substring(n - i)) && isScramble(s1.substring(i), s2.substring(0, n - i)))) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
