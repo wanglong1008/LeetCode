113438dc5921ce4dcb42318aa8648c7c880d1579
wanglong1008 <925367077@qq.com>
2021-04-17 12:18:11 +0800

    v0.1

---
+++ src/Question139.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Question139 {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        // memo[i] 表示 s 中以 i - 1 结尾的字符串是否可被 wordDict 拆分
+        boolean[] memo = new boolean[n + 1];
+        memo[0] = true;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (memo[j] && wordDict.contains(s.substring(j, i))) {
+                    memo[i] = true;
+                    break;
+                }
+            }
+        }
+        return memo[n];
+    }
+}
