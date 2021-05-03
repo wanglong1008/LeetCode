7af7d2ea3d110a6f496cd5b6883f8784a1daccde
wanglong1008 <925367077@qq.com>
2021-05-03 12:45:14 +0800

    v0.1

---
+++ src/Question235.java
@@ -0,0 +1,17 @@
+public class Question235 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (p.val==root.val){
+            return p;
+        }
+        if (q.val==root.val){
+            return q;
+        }
+        if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right,p,q);
+        }else if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left,p,q);
+        }else{
+            return root;
+        }
+    }
+}
