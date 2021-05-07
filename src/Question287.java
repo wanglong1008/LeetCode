d77e334e8da551431bda0891e535bce156889020
wanglong1008 <925367077@qq.com>
2021-05-07 12:00:16 +0800

    v0.1

---
+++ src/Question287.java
@@ -0,0 +1,17 @@
+public class Question287 {
+    public int findDuplicate(int[] nums) {
+        int fast = 0, slow = 0;
+        while(true) {
+            fast = nums[nums[fast]];
+            slow = nums[slow];
+            if(slow == fast) {
+                fast = 0;
+                while(nums[slow] != nums[fast]) {
+                    fast = nums[fast];
+                    slow = nums[slow];
+                }
+                return nums[slow];
+            }
+        }
+    }
+}
